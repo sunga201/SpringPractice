b55e58baa801fd60c48260b2f9f7491dc79b9b72
sunga201 <sung9255@gmail.com>
2020-12-27 13:38:55 +0900

    서비스 로직을 추상화하여 기존에 컨트롤러와 서비스 로직을 연결하는 부분을 제거함.

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -1,25 +1,18 @@
 package com.example.study.controller.api;
 
+import com.example.study.controller.CrudController;
+import com.example.study.model.entity.Item;
 import com.example.study.model.network.request.ItemApiRequest;
 import com.example.study.model.network.response.ItemApiResponse;
-import com.example.study.service.ItemApiLogicService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
 @RestController
 @RequestMapping("/api/item")
 @RequiredArgsConstructor
-public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse, Item> {
 
-    private final ItemApiLogicService itemApiLogicService;
-
-    @PostConstruct
-    public void init(){
-        this.baseService = itemApiLogicService;
-    }
 }
--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -1,25 +1,18 @@
 package com.example.study.controller.api;
 
+import com.example.study.controller.CrudController;
+import com.example.study.model.entity.User;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
-import com.example.study.service.UserApiLogicService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-public class UserApiController extends CrudController<UserApiRequest, UserApiResponse> {
+public class UserApiController extends CrudController<UserApiRequest, UserApiResponse, User> {
 
-    private final UserApiLogicService userApiLogicService;
-
-    @PostConstruct
-    protected void init(){
-        baseService = userApiLogicService;
-    }
 }
