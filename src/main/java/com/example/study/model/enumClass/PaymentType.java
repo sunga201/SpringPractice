4949b65dc1565560eb4cdf57146e19a04d765885
sunga201 <sung9255@gmail.com>
2020-12-27 23:18:45 +0900

    PaymentType enum에 POINT 추가(포인트 결제).

--- src/main/java/com/example/study/model/enumClass/PaymentType.java
+++ src/main/java/com/example/study/model/enumClass/PaymentType.java
@@ -5,9 +5,10 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum OrderGroupPaymentType {
-    CASH(0, "현금결제", "현금 결제"),
-    CARD(1, "신용카드", "신용카드 결제")
+public enum PaymentType {
+    CASH(0, "현금결제", "현금을 사용한 결제"),
+    CARD(1, "신용카드", "신용카드를 사용한 결제"),
+    POINT(2, "포인트 결제", "포인트를 사용한 결제")
     ;
 
     private int id;
