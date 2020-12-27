30f37830a020b28cde49dd9a7f8741abf5268ccb
sunga201 <sung9255@gmail.com>
2020-12-27 13:41:28 +0900

    서비스 로직을 추상화하여 기존에 컨트롤러와 서비스 로직을 연결하는 부분을 제 거함.

--- src/main/java/com/example/study/controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -4,13 +4,10 @@ import com.example.study.controller.CrudController;
 import com.example.study.model.entity.OrderGroup;
 import com.example.study.model.network.request.OrderGroupApiRequest;
 import com.example.study.model.network.request.OrderGroupApiResponse;
-import com.example.study.service.OrderGroupApiLogicService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.PostConstruct;
 
 @Slf4j
 @RestController
